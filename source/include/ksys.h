6f07860ca55cbac262c03cf2e90da957dcacf7e3
turbocat2001 <maximlogaev2001ezro@gmail.com>
2021-02-03 22:00:05 +0300

    Delete gui syscalls

--- source/include/ksys.h
+++ source/include/ksys.h
@@ -10,17 +10,6 @@ typedef struct {
     void* func_ptr;
 }coff_export_table;
 
-void _ksys_begin_draw();
-void _ksys_end_draw();
-void _ksys_create_window(int x, int y, int w, int h, const char *name, unsigned workcolor, unsigned style);
-void _ksys_change_window(int new_x, int new_y, int new_w, int new_h);
-void _ksys_define_button(unsigned x, unsigned y, unsigned w, unsigned h, unsigned id, unsigned color);
-void _ksys_draw_line(int xs, int ys, int xe, int ye, unsigned color);
-void _ksys_draw_bar(int x, int y, int w, int h, unsigned color);
-void _ksys_draw_bitmap(void *bitmap, int x, int y, int w, int h);
-void _ksys_draw_text(const char *text, int x, int y, int len, unsigned color);
-void _ksys_draw_text_bg(const char *text, int x, int y, int len, unsigned color, unsigned bg);
-
 //Retrieving an export table from a dynamic coff library
 coff_export_table* _ksys_cofflib_load(const char* path); 
 
