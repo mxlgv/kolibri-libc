9f4bff30c5e674f1bdbcb6177af99b80a94996da
turbocat2001 <maximlogaev2001ezro@gmail.com>
2021-02-07 20:26:34 +0300

    delete ksys dir

--- source/ksys/clock.c
+++
@@ -1,8 +0,0 @@
-#include <ksys.h>
-
-unsigned _ksys_get_clock()
-{
-    unsigned val;
-    asm_inline("int $0x40":"=a"(val):"a"(3));
-    return val;
-}
--- source/ksys/exit.c
+++
@@ -1,7 +0,0 @@
-#include <ksys.h>
-
-void _ksys_exit()
-{
-    asm_inline("int $0x40"::"a"(-1));
-}
- 
