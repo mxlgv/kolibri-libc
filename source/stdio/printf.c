67504c5558ccdfffabd9ea6d5a8289d63063badd
turbocat2001 <maximlogaev2001ezro@gmail.com>
2021-02-10 18:02:07 +0300

    [STDIO] Use conio for printf

--- source/stdio/printf.c
+++ source/stdio/printf.c
@@ -1,11 +1,23 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
 #include "format_print.h"
 
-int printf(const char* format, ...)
+int printf(const char *format, ...)
 {
-  va_list va;
-  va_start(va, format);
-  char buffer[1];
-  const int ret = _vsnprintf(_out_char, buffer, (size_t)-1, format, va);
-  va_end(va);
-  return ret;
+  va_list arg;
+  va_start(arg, format);
+  return vprintf(format, arg);
+}
+
+int vprintf ( const char * format, va_list arg )
+{
+  int len = 0;
+  char *s=NULL;
+  con_init();
+  s = malloc(4096);
+  len = vsnprintf(s, 4096, format, arg);
+  con_write_string(s, len);
+  free(s);
+  return(len);
 }
