686d04283a648fbbe8d83b8d0697c0ee6a5226a3
turbocat2001 <maximlogaev2001ezro@gmail.com>
2021-02-10 03:41:33 +0300

    [TESTS] Tests used conio.h

--- tests/argtest.c
+++ tests/argtest.c
@@ -1,11 +1,6 @@
 #include <ksys.h>
 #include <string.h>
-
-#define cdecl   __attribute__ ((cdecl))
-#define stdcall __attribute__ ((stdcall))
- 
-void stdcall (*con_init)(int wnd_width, int  wnd_height, int scr_width, int scr_height, const char* title);
-void stdcall (*con_write_asciiz)(const char* str);
+#include <conio.h>
 
 void reverse(char s[]) {
     int i, j; char c;
@@ -24,13 +19,8 @@ void itoa(int n, char s[]) {
 int main(int argc, char** argv) {
     char buf[16];
     int i;
-
-    coff_export_table *conlib  = _ksys_cofflib_load("/sys/lib/console.obj");
-    con_init   =      _ksys_cofflib_getproc(conlib, "con_init");
-    con_write_asciiz = _ksys_cofflib_getproc(conlib, "con_write_asciiz");
     
-    con_init(-1, -1, -1, -1, "argtest");
-    
+    con_init_console_dll();    
     con_write_asciiz("argc = ");
     itoa(argc, buf);
     con_write_asciiz(buf);
--- tests/mathtest.c
+++ tests/mathtest.c
@@ -1,28 +1,12 @@
 #include <ksys.h>
+#include <conio.h>
 #include <math.h>
 
-#define cdecl   __attribute__ ((cdecl))
-#define stdcall __attribute__ ((stdcall))
- 
-void stdcall (*con_init)(int wnd_width, int  wnd_height, int scr_width, int scr_height, const char* title);
-void stdcall (*puts)(const char* str);
-int cdecl (*printf)(const char* format, ...);
-
 int main() {
-	coff_export_table *conlib  = _ksys_cofflib_load("/sys/lib/console.obj");
-	con_init   =      _ksys_cofflib_getproc(conlib, "con_init");
-	puts = _ksys_cofflib_getproc(conlib, "con_write_asciiz");
-	printf = _ksys_cofflib_getproc(conlib, "con_printf");
-
-	con_init(-1, -1, -1, -1, "mathtest");
-    
-	puts("Math testing:\n");
-	puts("sqrt(25) = ");
-	int a = sqrt(25);
-	printf("%d\n", a);
-	
-	puts("pow(2, 2) = ");
-	a = pow(2, 2);
-	printf("%d\n", a);
-	puts("Done...");
+	con_init_console_dll();
+	con_write_asciiz("Math testing:\n");
+	con_printf("sqrt(25) = %d\n", (int)sqrt(25));
+	con_printf("pow(2, 2) = %d\n", (int)pow(2, 2));
+	con_exit(0);
+	return 0;
 }
