a8bf1c893d5e9b7f1910879388944d71ad30b569
turbocat2001 <maximlogaev2001ezro@gmail.com>
2021-02-10 18:01:02 +0300

    [INCLUDE] Delete not relising func

--- source/include/stdio.h
+++ source/include/stdio.h
@@ -36,12 +36,14 @@
 #include <stddef.h>
 #include <ksys.h>
 
-void putchar(char character);
+int  puts(const char *str);
 int  printf(const char* format, ...);
 int  sprintf(char* buffer, const char* format, ...);
 int  snprintf(char* buffer, size_t count, const char* format, ...);
 int  vsnprintf(char* buffer, size_t count, const char* format, va_list va);
 int  vprintf(const char* format, va_list va);
-int  fctprintf(void (*out)(char character, void* arg), void* arg, const char* format, ...);
+
+void debug_printf(const char* format, ...);
+
 
 #endif  // _STDIO_H_
