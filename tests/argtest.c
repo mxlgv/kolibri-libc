4b1eefee39fd26bcbcf007e90768194babedbb23
turbocat2001 <maximlogaev2001ezro@gmail.com>
2021-02-10 22:14:51 +0300

    [TESTS] Argtest: added crash-test gcc

--- tests/argtest.c
+++ tests/argtest.c
@@ -2,31 +2,15 @@
 #include <string.h>
 #include <stdio.h>
 
-void reverse(char s[]) {
-    int i, j; char c;
-    for (i = 0, j = strlen(s)-1; i<j; i++, j--) { c = s[i]; s[i] = s[j]; s[j] = c; }
-}
-
-void itoa(int n, char s[]) {
-    int i, sign; i = 0;
-    if ((sign = n) < 0) n = -n;
-    do { s[i++] = n % 10 + '0'; } while ((n /= 10) > 0); 
-    if (sign < 0) s[i++] = '-';
-    s[i] = '\0';
-    reverse(s);
-}
-
 int main(int argc, char** argv)
 {
-    char buf[16];
-    int i;
-    itoa(argc, buf);
-    printf("argc = %s\n", buf);
-    printf("Argument array:\n");
+    char test_stack[10000];
 
-    for (i = 0; i < argc; i++) {
-        itoa(i, buf);
-        printf("argv[%s] = %d\n", argv[i]);
+    printf("argc = %d\n", argc);
+    puts("Argument array:");
+
+    for (int i = 0; i < argc; i++) {
+        printf("argv[%d] = %s\n", i, argv[i]);    
     }
     debug_printf("Done!\n");
     return 0;
