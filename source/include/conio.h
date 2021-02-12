b9e91f55f361f06479f77f3fc6eded03efccbba5
mkostoevr <mkostoevr@yandex.ru>
2021-02-12 22:52:41 +0300

    [STDIO][CONIO] Fix internal header

--- source/include/conio.h
+++ source/include/conio.h
@@ -16,45 +16,45 @@ Adapted for tcc by Siemargl, 2016
 console.obj exports the following functions
 */
 
-extern void stdcall (*con_exit)(int bCloseWindow);
+extern void stdcall (*__con_exit)(int bCloseWindow);
 /* 	You should call this funstion at the end of the program.
 If bCloseWindow is zero, the string "[Finished]" will be added to the caption of 
 the window and the console window will remain on the screen until the user
 closes it. */
 
-extern void stdcall (*con_set_title)(const char* title);
+extern void stdcall (*__con_set_title)(const char* title);
 /* 	Set new window caption. */
 
-extern void stdcall (*con_write_asciiz)(const char* str);
+extern void stdcall (*__con_write_asciiz)(const char* str);
 /*	Display ASCIIZ-string to the console at the current position, shifting
 the current position. */
 
-extern void stdcall (*con_write_string)(const char* str, unsigned length);
-/* 	Similar to con_write_asciiz, but length of the string must be given as a 
+extern void stdcall (*__con_write_string)(const char* str, unsigned length);
+/* 	Similar to __con_write_asciiz, but length of the string must be given as a 
 separate parameter */
 
-extern int cdecl (*con_printf)(const char* format, ...);
+extern int cdecl (*__con_printf)(const char* format, ...);
 /* 	Standard "printf" function from ANSI C. */
 
-extern unsigned stdcall (*con_get_flags)(void);
+extern unsigned stdcall (*__con_get_flags)(void);
 /*	Get output flags. */
 
-extern unsigned stdcall (*con_set_flags)(unsigned new_flags);
+extern unsigned stdcall (*__con_set_flags)(unsigned new_flags);
 /*	Set output flags. This function returns previous values. */
 
 /* Flags (bitmask): */
 /* text color */
-#define CON_COLOR_BLUE		0x01
-#define CON_COLOR_GREEN	0x02
-#define CON_COLOR_RED		0x04
-#define CON_COLOR_BRIGHT	0x08
+#define _CON_COLOR_BLUE		0x01
+#define _CON_COLOR_GREEN	0x02
+#define _CON_COLOR_RED		0x04
+#define _CON_COLOR_BRIGHT	0x08
 /* background color */
-#define CON_BGR_BLUE		0x10
-#define CON_BGR_GREEN		0x20
-#define CON_BGR_RED		0x40
-#define CON_BGR_BRIGHT		0x80
+#define _CON_BGR_BLUE		0x10
+#define _CON_BGR_GREEN		0x20
+#define _CON_BGR_RED		0x40
+#define _CON_BGR_BRIGHT		0x80
 /* output controls */
-#define CON_IGNORE_SPECIALS	0x100
+#define _CON_IGNORE_SPECIALS	0x100
 /* if this flag is cleared, function interprets special characters:
 10 ('\n') - next line
 13 ('\r') - carriage return
@@ -95,24 +95,24 @@ otherwise, these characters will be displayed like ordinary characters. */
 	Esc[<number>D - move cursor to <number> positions left
 */
 /* signal "console closed"; appeared in version 6;
-	ignored by con_set_flags */
-#define CON_WINDOW_CLOSED 0x200
+	ignored by __con_set_flags */
+#define _CON_WINDOW_CLOSED 0x200
 /* The default value for flags = 7. (grey text on black background) */
 
-extern int stdcall (*con_get_font_height)(void);
+extern int stdcall (*__con_get_font_height)(void);
 /*	Get the height of the font. */
 
-extern int stdcall (*con_get_cursor_height)(void);
+extern int stdcall (*__con_get_cursor_height)(void);
 /*	Get the height of the cursor. */
 
-extern int stdcall (*con_set_cursor_height)(int new_height);
+extern int stdcall (*__con_set_cursor_height)(int new_height);
 /* 	Set the height of the cursor. This function returns previous value.
 An attempt to set the value out of the correct interval (from 0 to 
 font_height-1) is ignored.
 Cursor with zero height isn't displayed.
 Default value: - 15% from  font height. */
 
-extern int stdcall (*con_getch)(void);
+extern int stdcall (*__con_getch)(void);
 /*	Get one character from the keyboard. 
 
 For normal characters function returns ASCII-code. For extended 
@@ -121,18 +121,18 @@ and second call returns the extended code (similar to the DOS-function
 input). Starting from version 7, after closing the console window,
 this function returns 0. */
 
-extern short stdcall (*con_getch2)(void);
+extern short stdcall (*__con_getch2)(void);
 /*	Reads a character from the keyboard. Low byte contains the ASCII-code 
 (0 for extended characters), high byte - advanced code (like in BIOS 
 input functions). Starting from version 7, after closing the console
 window, this function returns 0. */
 
-extern int stdcall (*con_kbhit)(void);
+extern int stdcall (*__con_kbhit)(void);
 /*	Returns 1 if a key was pressed, 0 otherwise. To read pressed keys use
-con_getch and con_getch2. Starting from version 6, after closing 
+__con_getch and __con_getch2. Starting from version 6, after closing 
 the console window, this function returns 1. */
 
-extern char* stdcall (*con_gets)(char* str, int n);
+extern char* stdcall (*__con_gets)(char* str, int n);
 /*	Reads a string from the keyboard. Reading is interrupted when got
 "new line" character, or after reading the (n-1) characters (depending on 
 what comes first). In the first case the newline is also recorded in the
@@ -140,10 +140,10 @@ str. The acquired line is complemented by a null character.
 Starting from version 6, the function returns a pointer to the entered
 line if reading was successful, and NULL if the console window was closed. */
 
-typedef int (stdcall * con_gets2_callback)(int keycode, char** pstr, int* pn, 
+typedef int (stdcall * __con_gets2_callback)(int keycode, char** pstr, int* pn, 
 	int* ppos);
 
-extern char* stdcall (*con_gets2)(con_gets2_callback callback, char* str, int n);
+extern char* stdcall (*__con_gets2)(__con_gets2_callback callback, char* str, int n);
 /*	Con_gets completely analogous, except that when the user
 press unrecognized key, it calls the specified callback-procedure
 (which may, for example, handle up / down for history and tab to enter
@@ -158,26 +158,26 @@ to display it; 3 = immediately exit the function.
 Starting from version 6, the function returns a pointer to the entered
 line with the successful reading, and NULL if the console window was closed. */
 
-extern void stdcall (*con_cls)();
+extern void stdcall (*__con_cls)();
 /*	Clear screen and set cursor at upper left corner. */
 
 
-extern void stdcall (*con_get_cursor_pos)(int* px, int* py);
+extern void stdcall (*__con_get_cursor_pos)(int* px, int* py);
 /*	Wrote current (x) coordinate of cursor to *px, and (y) to *py. */
 
-extern void stdcall (*con_set_cursor_pos)(int x, int y);
+extern void stdcall (*__con_set_cursor_pos)(int x, int y);
 /*	Set the cursor position to the specified coordinates. If any of the
 parameters beyond the relevant range (from 0 to 1 scr_width-
 for x, from 0 to 1 for scr_height-y, scr_width scr_height and were asked if
-call con_init), then the corresponding coordinate of the cursor does not change.
+call __con_init), then the corresponding coordinate of the cursor does not change.
 */
 
-extern int con_is_load; 
-extern unsigned *con_dll_ver;
+extern int __con_is_load; 
+extern unsigned *__con_dll_ver;
 
-extern int con_init(void);
+extern int __con_init(void);
 
-extern int con_init_opt(int wnd_width, int wnd_height, int scr_width, int scr_height, const char* title);
+extern int __con_init_opt(int wnd_width, int wnd_height, int scr_width, int scr_height, const char* title);
 
 
 #endif
