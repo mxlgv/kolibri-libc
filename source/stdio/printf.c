b10b96e0c7fd47e42d557bca08c37dcf4d1dca47
mkostoevr <mkostoevr@yandex.ru>
2021-02-12 21:51:54 +0300

    [STDIO] Split printf.c into two files

--- source/stdio/printf.c
+++ source/stdio/printf.c
@@ -9,15 +9,3 @@ int printf(const char *format, ...)
   va_start(arg, format);
   return vprintf(format, arg);
 }
-
-int vprintf ( const char * format, va_list arg )
-{
-  int len = 0;
-  char *s=NULL;
-  con_init();
-  s = malloc(4096);
-  len = vsnprintf(s, 4096, format, arg);
-  con_write_string(s, len);
-  free(s);
-  return(len);
-}
---
+++ source/stdio/vprintf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include "format_print.h"
+
+int vprintf ( const char * format, va_list arg )
+{
+  int len = 0;
+  char *s=NULL;
+  con_init();
+  s = malloc(4096);
+  len = vsnprintf(s, 4096, format, arg);
+  con_write_string(s, len);
+  free(s);
+  return(len);
+}
