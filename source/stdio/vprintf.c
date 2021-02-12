e5f969d253c87e0668bae2614a04afb318985bd7
mkostoevr <mkostoevr@yandex.ru>
2021-02-12 23:27:02 +0300

    [STDIO] Allocate buffer on stack

--- source/stdio/vprintf.c
+++ source/stdio/vprintf.c
@@ -7,11 +7,10 @@
 int vprintf ( const char * format, va_list arg )
 {
   int len = 0;
-  char *s=NULL;
+  char s[4096];
+
   __con_init();
-  s = malloc(4096);
   len = vsnprintf(s, 4096, format, arg);
   __con_write_string(s, len);
-  free(s);
   return(len);
 }
