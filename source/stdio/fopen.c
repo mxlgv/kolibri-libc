743fd4ead0f7f81d7c36b687ab66699eceb7cc2a
mkostoevr <mkostoevr@yandex.ru>
2021-02-14 16:28:51 +0300

    [STDIO] Fix printf()

--- source/stdio/fopen.c
+++ source/stdio/fopen.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-FILE *fopen(const char *restrict name, const char *restrict mode) {
+static char *strdup(char *str) {
+    char *buf = malloc(strlen(str) + 1);
+    buf[strlen(str)] = 0;
+    strcpy(buf, str);
+    return buf;
+}
+
+FILE *fopen(const char *restrict _name, const char *restrict _mode) {
+    BDFE_struct info;
+    if (_ksys_file_get_info(_name, &info)) {
+        return NULL;
+    }
+
     FILE *out = malloc(sizeof(FILE));
 
     if (!out) {
         return NULL;
     }
 
-    out->name = name;
+    out->name = strdup(_name);
     out->position = 0;
     out->error = 0;
     out->eof = 0;
@@ -15,18 +28,14 @@ FILE *fopen(const char *restrict name, const char *restrict mode) {
     out->orientation = 0;
     out->mode = 0;
 
-    if (strchr(mode, 'b')) { mode |= _STDIO_F_B; }
-    if (strchr(mode, 'x')) { mode |= _STDIO_F_X; }
-    if (strchr(mode, 'a')) { mode |= _STDIO_F_A; }
-    if (strchr(mode, 'r')) { mode |= _STDIO_F_R; }
-    if (strchr(mode, 'w')) { mode |= _STDIO_F_W; }
-    if (strchr(mode, '+')) { mode |= _STDIO_F_R | _STDIO_F_W; }
+    if (strchr(_mode, 'b')) { out->mode |= _STDIO_F_B; }
+    if (strchr(_mode, 'x')) { out->mode |= _STDIO_F_X; }
+    if (strchr(_mode, 'a')) { out->mode |= _STDIO_F_A; }
+    if (strchr(_mode, 'r')) { out->mode |= _STDIO_F_R; }
+    if (strchr(_mode, 'w')) { out->mode |= _STDIO_F_W; }
+    if (strchr(_mode, '+')) { out->mode |= _STDIO_F_R | _STDIO_F_W; }
 
-    if (mode |= _STDIO_F_A) {
-        BDFE_struct info;
-        if (_ksys_file_get_info(out->name, &info)) {
-            return NULL;
-        }
+    if (out->mode & _STDIO_F_A) {
         out->position = info.size;
         out->append_offset = info.size;
     }
